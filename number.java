4744684e937e2c0c95589468716160582a293059
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-04-29 19:06:27 +0800

    modify print

--- number.java
+++ number.java
@@ -3,34 +3,26 @@ public class number{
  
 	int DEF_DIV_SCALE = 10;
 
-	public double add(double v1,double v2){//add
 
-		BigDecimal b1 = new BigDecimal(Double.toString(v1));
-
-		BigDecimal b2 = new BigDecimal(Double.toString(v2));
-
-		return b1.add(b2).doubleValue();
-
+	public void add(String v1,String v2){//add
+		BigDecimal b1 = new BigDecimal(v1);
+		BigDecimal b2 = new BigDecimal(v2);
+		System.out.println("add:"+b1.add(b2));
 	}
 
 
+	public void sub(String v1,String v2){
 
-	public double sub(double v1,double v2){
-
-		BigDecimal b1 = new BigDecimal(Double.toString(v1));
-
-                BigDecimal b2 = new BigDecimal(Double.toString(v2));
-
-		return b1.subtract(b2).doubleValue();
+		BigDecimal b1 = new BigDecimal(v1);
+                BigDecimal b2 = new BigDecimal(v2);
+		System.out.println("sub:"+b1.subtract(b2));
 
 	}
 
-	public  double mul(double v1,double v2){
+	public void mul(String v1,String v2){
 
-		BigDecimal b1 = new BigDecimal(Double.toString(v1));
-
-		BigDecimal b2 = new BigDecimal(Double.toString(v2));
-
-		return b1.multiply(b2).doubleValue();
+		BigDecimal b1 = new BigDecimal(v1);
+		BigDecimal b2 = new BigDecimal(v2);
+		System.out.println("mul:"+b1.multiply(b2));
 
 	}
