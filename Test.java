1793c17e263e5dc7d0518b7f889a877664ba2a50
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-04-29 19:35:28 +0800

    add div

--- Test.java
+++ Test.java
@@ -1,20 +1,18 @@
+//u10316052
 import java.util.*;
 public class Test{
-
-
 	public static void main(String[] args){
-
 		number Number=new number();
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("please input two number\n Two number will add and sub and mul and div");
-		double a=scanner.nextDouble();
-		double b=scanner.nextDouble();
-		
-		System.out.println("add:"+Number.add(a,b));
-		System.out.println("sub:"+Number.sub(a,b));
-		System.out.println("mul:"+Number.mul(a,b));		
-		//System.out.println("div:"+Number.add(a,b));
-
-
+		String a=scanner.nextLine();
+		String b=scanner.nextLine();
+		System.out.println("please input you div After the Decimal point display x ? ");
+		int c=scanner.nextInt();
+		Number.setscale(c);
+		Number.add(a,b);
+		Number.sub(a,b);
+		Number.mul(a,b);
+		Number.div(a,b);
 	}
 }
